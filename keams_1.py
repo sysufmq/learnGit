b43d81d3ac1c3b4b4fae541edb6ba687209e5987
范茂青 <fanmaoqing0018@ynyc.com>
2021-01-05 20:59:53 +0800

    modified:   keams_1.py

--- keams_1.py
+++ keams_1.py
@@ -7,8 +7,9 @@ import numpy as np
 
 # 输入数据
 data = pd.read_csv('data.csv', encoding='gbk')
+
 train_x = data[["2019年国际排名", "2018世界杯", "2015亚洲杯"]]
-df = pd.DataFrame(train_x)
+
 kmeans = KMeans(n_clusters=4)
 # 规范化到[0,1]空间
 min_max_scaler = preprocessing.MinMaxScaler()
