69f832eb92bfa6b6fa4a794e0816c6ae6f582261
范茂青 <fanmaoqing0018@ynyc.com>
2021-01-05 20:24:52 +0800

    On branch keam-1  Changes to be committed: 	new file:   data.csv 	deleted:    die_visual.py 	new file:   keams_1.py

---
+++ keams_1.py
@@ -0,0 +1,22 @@
+# coding: utf-8
+#对球队进行聚类
+from sklearn.cluster import KMeans
+from sklearn import preprocessing
+import pandas as pd
+import numpy as np
+
+# 输入数据
+data = pd.read_csv('data.csv', encoding='gbk')
+train_x = data[["2019年国际排名", "2018世界杯", "2015亚洲杯"]]
+df = pd.DataFrame(train_x)
+kmeans = KMeans(n_clusters=3)
+# 规范化到[0,1]空间
+min_max_scaler = preprocessing.MinMaxScaler()
+train_x = min_max_scaler.fit_transform(train_x)
+# kmeans算法
+kmeans.fit(train_x)
+predict_y = kmeans.predict(train_x)
+# 合并聚类结果，插入到原数据中
+result = pd.concat((data, pd.DataFrame(predict_y)), axis=1)
+result.rename({0: u'聚类'}, axis=1, inplace=True)
+print(result)
